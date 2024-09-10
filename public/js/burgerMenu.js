78a66b12f35f5d14d4201b1556f827eb5035c02c
Arsen <136583145+burkoA@users.noreply.github.com>
2024-09-10 14:42:01 +0000

    Update

--- public/js/burgerMenu.js
+++ public/js/burgerMenu.js
@@ -1,15 +1,6 @@
 const menuBtn = document.getElementById('menu-btn');
 const mobileMenu = document.getElementById('mobile-menu');
-let isMenuVisible = false;
 
 menuBtn.addEventListener('click', () => {
-    isMenuVisible = !isMenuVisible;
-
-    if (isMenuVisible) {
-        mobileMenu.classList.remove('hidden');
-        document.body.style.paddingBottom = `${mobileMenu.offsetHeight}px`;
-    } else {
-        mobileMenu.classList.add('hidden');
-        document.body.style.paddingBottom = '0';
-    }
+    mobileMenu.classList.toggle('hidden');
 });
